a4d241aeb9eadd94506c6f28f3a4ae3774b6c843
chenming c <43266446+chen622@users.noreply.github.com>
2020-10-19 23:44:40 +0800

    feat: change the tips and write to a file

--- KeyboardMap/keyboard.py
+++ KeyboardMap/keyboard.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import time
 
 key_map = [
     # line 1
@@ -18,41 +19,53 @@ key_map = [
 ]
 
 
-def keyboard_map(dataset):
+def keyboard_map(dataset_param):
     print_map()
     result = {}
-    i = 0
-    for data in dataset:
+    j = 0
+    time_start = time.time()
+    for data in dataset_param:  # 遍历全部密码
         aim_str = ''
         last_index = (-2, -2)
-        for char in data:
+        for char in data:  # 遍历密码字符串
             x, y = find_index(char)
-            if (x, y) != (-2, -2):
-                distance_x = 0.5 * (x - last_index[0])
-                distance_y = y - last_index[1]
-                distance = distance_x * distance_x + distance_y * distance_y
-                if distance < 2:
-                    aim_str += char
-                else:
-                    if len(aim_str) >= 3:
-                        if aim_str in result:
-                            result[aim_str] += 1
-                        else:
-                            result[aim_str] = 1
-                    aim_str = char
+            # 计算和上一个位置的差值
+            distance_x = 0.5 * (x - last_index[0])
+            distance_y = y - last_index[1]
+            distance = distance_x * distance_x + distance_y * distance_y
+            if distance < 2 and x != -2:  # 如果邻位则加上
+                aim_str += char
+                # if len(aim_str) >= 3:
+                #     if aim_str in result:
+                #         result[aim_str] += 1
+                #     else:
+                #         result[aim_str] = 1
+            else:
+                if len(aim_str) >= 3:
+                    if aim_str in result:
+                        result[aim_str] += 1
+                    else:
+                        result[aim_str] = 1
+                aim_str = char
             last_index = (x, y)
         if len(aim_str) >= 3:
             if aim_str in result:
                 result[aim_str] += 1
             else:
                 result[aim_str] = 1
-        i += 1
-        if i % 100000 == 0:
-            print(f'Analyze data {i}/{len(dataset)}')
+        j += 1
+        if j % 100000 == 0:
+            time_end = time.time()
+            percent = j / len(dataset_param) * 100
+            print(f'Analyze 10W lines within {(time_end - time_start):.1f} seconds! {percent:.2f}%')
+            time_start = time_end
     return result
 
 
+# 拿到char在键盘里的index
 def find_index(char):
+    if char.isalpha():
+        char = char.lower()
     for y in range(len(key_map)):
         for x in range(len(key_map[y])):
             for symbol in key_map[y][x]:
@@ -62,7 +75,7 @@ def find_index(char):
 
 
 def print_map():
-    print('The key map which I used is like this:')
+    print('\nThe key map which I used is like this:')
     for y in range(len(key_map)):
         for x in range(len(key_map[y])):
             if key_map[y][x][0] == '-1':
@@ -70,23 +83,33 @@ def print_map():
             else:
                 print(key_map[y][x][0], end='')
         print()
+    print()
 
 
 if __name__ == "__main__":
-    data_file = open('../www.csdn.net.sql', 'r+', encoding='utf-8', errors='ignore')
-    dataset = []
-    line = data_file.readline()
-    i = 0
-    while line:
-        str_s = line.split(' # ')
-        if len(str_s) == 3:
-            dataset.append(str_s[1])
-        else:
-            print(f'error data: {line}')
-        i += 1
-        if i % 100000 == 0:
-            print(f'Load file({len(dataset)} lines)!')
-            break
+    with open('../www.csdn.net.sql', 'r+', encoding='utf-8', errors='ignore') as data_file:
+        dataset = []
         line = data_file.readline()
-    print(f'Load file({len(dataset)} lines) complete!')
-    print(keyboard_map(dataset))
+        i = 0
+        while line:
+            str_s = line.split(' # ')
+            if len(str_s) == 3:
+                dataset.append(str_s[1])
+            else:
+                print(f'error data: {line}')
+            i += 1
+            if i % 1000000 == 0:
+                print(f'Load {len(dataset)} lines!')
+                # break
+            line = data_file.readline()
+        print(f'Load file({len(dataset)} lines) complete!')
+    result_map = keyboard_map(dataset)
+    result_list = result_map.items()
+    order_list = sorted(result_list, key=lambda item: item[1], reverse=True)
+    with open('result.dat', 'w', encoding='utf-8', errors='ignore') as out_file:
+        i = 0
+        for item in order_list:
+            out_file.write(f'{item[0]} {item[1]}\n')
+            i += 1
+            if i % 1000:
+                out_file.flush()
